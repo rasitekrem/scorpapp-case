5435514e7550fcf5d54e5b015029dbcea631d755
Rasit Ekrem Ataklı <rasitatakli42@gmail.com>
2021-08-15 12:13:44 +0300

    Create AppUserControl.vue

---
+++ src/components/AppUserControl.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    v-if="!isMobile"
+    class="
+      d-flex
+      justify-content-between
+      align-items-center
+      order-md-last order-first
+    "
+  >
+    <v-btn v-if="!user" class="px-5 mr-2" color="success" small
+      >Giriş Yap</v-btn
+    >
+    <v-btn v-else class="px-5" color="warning" small>Çıkış Yap</v-btn>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    isMobile: {
+      type: Boolean,
+      default: false,
+    },
+    user: {
+      type: Object,
+      default: () => null,
+    },
+  },
+};
+</script>
